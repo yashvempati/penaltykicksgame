9e070cd25b8c32f2b10851056f465a98297baaa1
Yash V <72429905+yashvempati@users.noreply.github.com>
2024-04-05 09:44:18 +0530

    Update penaltykicks.py
    I removed a loop that didn't really need to be there

--- penaltykicks.py
+++ penaltykicks.py
@@ -1,4 +1,3 @@
-## I hate Pylint very much
 import random
 
 print("Welcome to the penalty shootout!")  
@@ -10,17 +9,16 @@ print(f"{player_name}, can you beat the keeper?")
 print(" ◯")
 print("|_|")
 
-game_on = True
-while game_on:
-    keeper_position = random.randint(1, 3)
-    guessed_position = input("Will you go left (1), right (3) or down the middle (2)? ")
 
-    if guessed_position != keeper_position:
+keeper_position = random.randint(1, 3)
+guessed_position = input("Will you go left (1), right (3) or down the middle (2)? ")
+
+if guessed_position != keeper_position:
         print("'Oh, wonderful! What a goal! And what a time, in what a place! What a player!'- Peter Drury")
-        game_on = False  ## End game because like a goal was scored yeah?
-    else:
+        game_on = False
+else:
         print("'The end. The end. Make sense of that. Where on Earth are we now? A night of dismay.'- Peter Drury.")
-        game_on = False  ## Still end game, you don't get retries
+        game_on = False
 
-## Print this line after the game loop finishes
-print("Thanks for playing! Ab chal jake padhai kar")
+## Print this line after the game finishes
+print("Thanks for playing!")
